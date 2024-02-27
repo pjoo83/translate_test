d9a5602273524a1fad5ebd3a4f64cdec40377af5
changhao <hao.chang@ushow.media>
2024-02-27 12:44:07 +0800

    干掉多余打印

--- base/check_tools.py
+++ base/check_tools.py
@@ -174,7 +174,7 @@ def generate_xlsx(file, file_list, msg, msg2, channel, datas):
     sheet.append(head)
     for file in file_list:
         for i in file:
-            print(i)
+            # print(i)
             sheet.append(i)
         if msg2:
             sheet.append(msg2)
