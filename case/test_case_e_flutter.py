f2944579d1e9dc7ab01262ec0fd7207a7c86ca05
pjoo83 <pjoo83@163.com>
2024-05-15 15:35:16 +0800

    增加绝对路径方法

--- case/test_case_e_flutter.py
+++ case/test_case_e_flutter.py
@@ -1,13 +1,13 @@
 from base.check_tools import start_check
 from base.read_all_files import find_file
-
+from base.check_tools import absolute_path
 
 def test_check():
     """
             服务端内容检查
         """
     # self.getDriver.close_page()
-    files = find_file(r"D:\project\starx_project\translate\data\flutter_data", include_str="language_flutter",
+    files = find_file(fr"{absolute_path('flutter_data')}", include_str="language_flutter",
                       filter_strs=[".~"])
     if len(files) > 1:
 
