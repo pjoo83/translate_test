ddb5dfb3112a5698fb23233d325d88af089bbfb0
changhao <hao.chang@ushow.media>
2024-05-15 15:00:51 +0800

    改为pytest

--- run_all_case.py
+++ run_all_case.py
@@ -1,25 +1,30 @@
 # coding=utf-8
 import time
-import unittest
-from unittest import defaultTestLoader
-
-# 测试用例存放路径
-case_path = 'case'
-
-
-# 获取所有测试用例
-def get_allcase():
-    """
-    执行所有测试文件
-    """
-    discover = defaultTestLoader.discover(case_path, pattern="test*.py")
-    suite = unittest.TestSuite()
-    suite.addTest(discover)
-    return suite
-
-
+# import unittest
+# from unittest import defaultTestLoader
 #
-if __name__ == '__main__':
-    # 运行测试用例
-    runner = unittest.TextTestRunner()
-    runner.run(get_allcase())
+# # 测试用例存放路径
+# case_path = 'case'
+#
+#
+# # 获取所有测试用例
+# def get_allcase():
+#     """
+#     执行所有测试文件
+#     """
+#     discover = defaultTestLoader.discover(case_path, pattern="test*.py")
+#     suite = unittest.TestSuite()
+#     suite.addTest(discover)
+#     return suite
+#
+#
+# #
+# if __name__ == '__main__':
+#     # 运行测试用例
+#     runner = unittest.TextTestRunner()
+#     runner.run(get_allcase())
+
+import os
+import pytest
+
+pytest.main(["-s", r"D:\project\starx_project\translate\case"])
