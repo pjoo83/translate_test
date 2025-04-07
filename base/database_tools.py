f9f9ebec90675ab05db2c2b56709122731afa39a
pjoo83 <pjoo83@163.com>
2025-04-07 18:15:57 +0800

    增加服务端新下载逻辑

--- base/database_tools.py
+++ base/database_tools.py
@@ -1,27 +1,44 @@
 import pymysql
+import datetime
 
 
-def execute_sql(channel_id, newly_quantity, modify_quantity, quantity):
+def execute_sql(channel_id, newly_quantity, modify_quantity, quantity, method):
     db = pymysql.connect(
         host='127.0.0.1',
         user='root',
         password='zxcv1234',
         database='localhost_interface'
     )
-    insert_execute_statistics = "insert into translate_statistics (channel_id,newly_quantity,modify_quantity,quantity) " \
-                                "values (%s,%s,%s,%s)"
-    values = channel_id, newly_quantity, modify_quantity, quantity
-    cursor = db.cursor()
-    try:
-        cursor.execute(insert_execute_statistics, values)
-        db.commit()
-        print('插入成功数据库成功')
-    except pymysql.MySQLError as e:
-        print(e, '插入失败')
+    if method == 'insert':
+        insert_execute_statistics = "insert into translate_statistics (channel_id,newly_quantity,modify_quantity,quantity) " \
+                                    "values (%s,%s,%s,%s)"
+        values = channel_id, newly_quantity, modify_quantity, quantity
+        cursor = db.cursor()
+        try:
+            cursor.execute(insert_execute_statistics, values)
+            db.commit()
+            print('插入成功数据库成功')
+        except pymysql.MySQLError as e:
+            print(e, '插入失败')
 
-    data1 = cursor.fetchone()
-    data2 = cursor.fetchall()
-    db.close()
+        data1 = cursor.fetchone()
+        data2 = cursor.fetchall()
+        db.close()
+    elif method == 'query':
+        select_execute_statistics =f"SELECT a.time from translate_statistics a INNER JOIN translate_channel " \
+                                   f"b on a.channel_id =b.channel_id where b.channel ='ios' ORDER BY time desc;   "
+        cursor = db.cursor()
+        try:
+            cursor.execute(select_execute_statistics)
+            result = cursor.fetchone()
+            print('查询结果', result)
+            dt = result[0]
+            next_day_midnight = (dt + datetime.timedelta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0)
+            timestamp_ms = int(next_day_midnight.timestamp() * 1000)
+            return timestamp_ms
+        except pymysql.MySQLError as e:
+            print(e, '查询错误')
+
 
 if __name__ == '__main__':
-    execute_sql(2, 30, 50, 900)
+    execute_sql(2, 30, 50, 900, 'query')
--- base/ef_download.py
+++ base/ef_download.py
@@ -1,59 +1,71 @@
 import requests
 import time
-import os
-from datetime import datetime, timezone
-
-
-# from base.check_tools import absolute_path
+from datetime import datetime, timezone, timedelta
+from database_tools import execute_sql
 
 
 def ef_download(channel, ids):
     utc_midnight = datetime.now(timezone.utc).replace(hour=0, minute=0, second=0, microsecond=0)
     timestamp_ms = int(utc_midnight.timestamp() * 1000)
-    url = 'https://ef-test.ushow.media/api-internal/phrase/translate/keys/tran/export?t=1742800404485&operator_id' \
-          '=856&operator_name=hao.chang&operator_email=hao.chang@ushow.media&language=zh'
-    payload = "{\"keys\":[],\"email\":\"hao.chang@ushow.media\"," \
-              f"\"platform\":{ids},\"create_start_time\":1741910400000," \
-              f"\"create_stop_time\":{timestamp_ms}}}"
+    url = "https://ef.ushow.media/api-internal/phrase/translate/keys/tran/export?t=1744010316350&operator_id=994&o" \
+          "perator_name=hao.chang&operator_email=hao.chang@ushow.media&language=zh"
     headers = {
         'accept': 'application/json, text/plain, */*',
         'accept-language': 'zh-CN,zh;q=0.9',
         'content-type': 'application/json;charset=UTF-8',
-        'cookie': 'thinkjs=a3fe8a3b-6626-49ec-8dab-be9a512656fb; router_name=History; EF_USER_VERIFY=; '
-                  'EF-Token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
-                  '.eyJ1c2VyX2lkIjozNjEsInRva2VuIjoiZXlKaGJHY2lPaUpJVXpJMU5pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmxlSEFpT2pF'
-                  'M05ETXlNek00TmpJc0ltbHpjeUk2SW5OemJ5SXNJbTVpWmlJNk1UYzBNamd3TVRnMU55d2lkWE5sY2w5cFpDSTZPRFUyTENKdVl'
-                  'XMWxJam9pYUdGdkxtTm9ZVzVuSWl3aVpXMWhhV3dpT2lKb1lXOHVZMmhoYm1kQWRYTm9iM2N1YldWa2FXRWlMQ0pzWVhKclgy'
-                  'NWhiV1VpT2lMbHVMam10YWtpTENKc1lYSnJYMkYyWVhSaGNpSTZJbWgwZEhCek9pOHZjekV0YVcxbWFXeGxMbVpsYVhOb2RXT'
-                  'mtiaTVqYjIwdmMzUmhkR2xqTFhKbGMyOTFjbU5sTDNZeEwzWXpYekF3YVRWZk5tTXdaV015WVRndE56TTNPQzAwTkRReUxUaz'
-                  'FNMlV0TnpjMk9EQXdPVEJoWWpSbmZqOXBiV0ZuWlY5emFYcGxQVGN5ZURjeVhIVXdNREkyWTNWMFgzUjVjR1U5WEhVd01EST'
-                  'JjWFZoYkdsMGVUMWNkVEF3TWpabWIzSnRZWFE5Y0c1blhIVXdNREkyYzNScFkydGxjbDltYjNKdFlYUTlMbmRsWW5BaUxDSn'
-                  'NZWEpyWDJkeWIzVndJam9pYzNSaGNpSXNJbTVsWldSZmMyRm1aVjlqYUdWamF5STZabUZzYzJVc0lteHZaMmx1WDNScGJXVW'
-                  'lPakUzTkRJNE1ERTROako5LkZPN0JtaDB0akh0WWktQ0pqT2dyanBVeHhYUmR6YlVyeVZNVVNUeElNeVEiLCJpYXQiOjE3ND'
-                  'I4MDE4NjMsImV4cCI6MTc0Mjg4ODI2M30.TaoVOYw9l_udfeDrt2cBqbAF7Rea-ntn9XxeJg4vp_k; EF_USER_EMAIL=hao'
-                  '.chang@ushow.media; EF_USER_NAME=hao.chang; sidebarStatus=0; thinkjs=d37bc577-d58e-4f7b-b878-d66'
-                  '1fc3aece9',
-        'operatorid': '361',
-        'origin': 'https://ef-test.ushow.media',
+        'operatorid': '994',
+        'origin': 'https://ef.ushow.media',
         'priority': 'u=1, i',
-        'referer': 'https://ef-test.ushow.media/translate/history',
-        'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+        'referer': 'https://ef.ushow.media/translate/history',
+        'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
         'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
+        'sec-ch-ua-platform': '"macOS"',
         'sec-fetch-dest': 'empty',
         'sec-fetch-mode': 'cors',
         'sec-fetch-site': 'same-origin',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
-                      ' Chrome/131.0.0.0 Safari/537.36'
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
+        'Cookie': '_ga_Y5QLWEHNZ4=GS1.1.1736474542.33.0.1736474542.60.0.0; _ga=GA1.2.1792866'
+                  '753.1713751715; thinkjs=3703e188-b933-499b-b241-973e8ab90555; router_name=Hi'
+                  'story; EF_USER_VERIFY=; EF-Token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2'
+                  'VyX2lkIjo5OTQsInRva2VuIjoiZXlKaGJHY2lPaUpJVXpJMU5pSXNJblI1Y0NJNklrcFhWQ0o5Lm'
+                  'V5SmxlSEFpT2pFM05EUTBNakk0TVRFc0ltbHpjeUk2SW5OemJ5SXNJbTVpWmlJNk1UYzBNems1TU'
+                  'Rnd05pd2lkWE5sY2w5cFpDSTZNVEUwTkN3aWJtRnRaU0k2SW1oaGJ5NWphR0Z1WnlJc0ltVnRZV'
+                  '2xzSWpvaWFHRnZMbU5vWVc1blFIVnphRzkzTG0xbFpHbGhJaXdpYkdGeWExOXVZVzFsSWpvaTVi'
+                  'aTQ1cldwSWl3aWJHRnlhMTloZG1GMFlYSWlPaUpvZEhSd2N6b3ZMM014TFdsdFptbHNaUzVtWlds'
+                  'emFIVmpaRzR1WTI5dEwzTjBZWFJwWXkxeVpYTnZkWEpqWlM5Mk1TOTJNMTh3TUdrMVh6WmpNR1Zq'
+                  'TW1FNExUY3pOemd0TkRRME1pMDVOVE5sTFRjM05qZ3dNRGt3WVdJMFozNF9hVzFoWjJWZmMybDZa'
+                  'VDAzTW5nM01seDFNREF5Tm1OMWRGOTBlWEJsUFZ4MU1EQXlObkYxWVd4cGRIazlYSFV3TURJMlptO'
+                  'XliV0YwUFhCdVoxeDFNREF5Tm5OMGFXTnJaWEpmWm05eWJXRjBQUzUzWldKd0lpd2liR0Z5YTE5bm'
+                  'NtOTFjQ0k2SW5OMFlYSWlMQ0p1WldWa1gzTmhabVZmWTJobFkyc2lPbVpoYkhObExDSnNiMmRwYm'
+                  'w5MGFXMWxJam94TnpRek9Ua3dOems0ZlEuR1pncnJYLU9IOVVDZ2dnenVTUjZrUzNwMWNaQ0oxUFB'
+                  'leWdIZjlfYkprbyIsImlhdCI6MTc0NDAxMDI5OSwiZXhwIjoxNzQ0MDk2Njk5fQ.T_XN9UH4jT4l-'
+                  'ZVLV82Dy_YoTbZKS6LgwkG8X82nsYk; EF_USER_EMAIL=hao.chang@ushow.media; EF_USER_'
+                  'NAME=hao.chang; sidebarStatus=0'
     }
-    # response = requests.request("POST", url, headers=headers, data=payload)
-    file_path = file_name(channel)
-    download_request = requests.post(headers=headers, url=url, verify=False, stream=True, data=payload)
-    file_url = download_request.json()['data']
-    response = requests.request("get", file_url, headers=headers, data=payload)
-    with open(file_path, "wb") as f:
-        f.write(response.content)
-    print(f"{channel}文件下载完成")
+    if channel =='server':
+        payload = "{\"keys\":[],\"email\":\"hao.chang@ushow.media\"," \
+                  f"\"platform\":{ids},\"create_start_time\":{execute_sql(0, 0, 0, 0, 'query')}," \
+                  f"\"create_stop_time\":{timestamp_ms}}}"
+        # response = requests.request("POST", url, headers=headers, data=payload)
+        file_path = file_name(channel)
+        download_request = requests.post(headers=headers, url=url, verify=False, stream=True, data=payload)
+        file_url = download_request.json()['data']
+        response = requests.request("get", file_url, headers=headers, data=payload)
+        with open(file_path, "wb") as f:
+            f.write(response.content)
+        print(f"{channel}文件下载完成")
+    else:
+        payload = "{\"keys\":[],\"email\":\"hao.chang@ushow.media\"," \
+                  f"\"platform\":{ids},\"create_start_time\":1741910400000," \
+                  f"\"create_stop_time\":{timestamp_ms}}}"
+        # response = requests.request("POST", url, headers=headers, data=payload)
+        file_path = file_name(channel)
+        download_request = requests.post(headers=headers, url=url, verify=False, stream=True, data=payload)
+        file_url = download_request.json()['data']
+        response = requests.request("get", file_url, headers=headers, data=payload)
+        with open(file_path, "wb") as f:
+            f.write(response.content)
+        print(f"{channel}文件下载完成")
 
 
 def file_name(channel):
@@ -61,6 +73,6 @@ def file_name(channel):
     new_name = f'/Users/lbj/Desktop/starx/project/translate_test/data/{channel}_data/{times}language_{channel}.xlsx'
     return new_name
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     ef_download('ios', 2)
